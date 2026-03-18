504e638067d38dee494f6e0585848cd2c3d4828a
morik <kosuke.mori.5120@gmail.com>
2026-03-18 21:14:07 +0900

    add swift

---
+++ apps/ios/SpeechAgent/SpeechAgent/SpeechAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechAgentApp.swift
+//  SpeechAgent
+//
+//  Created by Kosuke Mori on 2026/03/18.
+//
+
+import SwiftUI
+
+@main
+struct SpeechAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
